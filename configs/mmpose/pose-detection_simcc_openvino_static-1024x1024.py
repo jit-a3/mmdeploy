5148e3765a0f62a7e8efa1900944f94d99697d57
Jit Ray Chowdhury <jit.raychowdhury@airbus-sv.com>
2024-03-05 23:13:48 +0000

    additional configs

---
+++ configs/mmpose/pose-detection_simcc_openvino_static-1024x1024.py
@@ -0,0 +1,18 @@
+_base_ = ['./pose-detection_static.py', '../_base_/backends/openvino.py']
+
+backend_config = dict(
+    model_inputs=[dict(opt_shapes=dict(input=[1, 3, 1024, 1024]))])
+onnx_config = dict(
+    input_shape=[1024, 1024],
+    output_names=['simcc_x', 'simcc_y'],
+    dynamic_axes={
+        'input': {
+            0: 'batch',
+        },
+        'simcc_x': {
+            0: 'batch'
+        },
+        'simcc_y': {
+            0: 'batch'
+        }
+    })
